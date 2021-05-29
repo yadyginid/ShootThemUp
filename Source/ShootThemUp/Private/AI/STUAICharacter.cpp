08811bb05c8b5de28f5d753962b261eb73a4011c
yadyginid <yadyginid@gmail.com>
2021-05-29 13:25:55 +0300

    Create AI Character, AI controller

---
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -0,0 +1,10 @@
+// Shoot Them Up Game. All right reserved.
+
+#include "AI/STUAICharacter.h"
+#include "AI/STUAIController.h"
+
+ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit)
+{
+    AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+    AIControllerClass = ASTUAIController::StaticClass();
+}
