19d5c9915e4081531e79989c85167f1098217f15
yadyginid <yadyginid@gmail.com>
2021-05-29 16:08:13 +0300

    add Rotation Character with AIPerseption

--- Source/ShootThemUp/Public/AI/STUAIController.h
+++ Source/ShootThemUp/Public/AI/STUAIController.h
@@ -6,11 +6,21 @@
 #include "AIController.h"
 #include "STUAIController.generated.h"
 
+class USTUAIPerceptionComponent;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTUAIController : public AAIController
 {
     GENERATED_BODY()
 
+public:
+    ASTUAIController();
+
 protected:
     virtual void OnPossess(APawn* InPawn) override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    USTUAIPerceptionComponent* STUAIPerceptionComponent;
+
+    virtual void Tick(float DeltaTime) override;
 };
