4b9180a053d7f6ab5a95f723ffdeb4315be2964c
yadyginid <yadyginid@gmail.com>
2021-06-23 10:40:55 +0300

    Get Location Point for move with EQS

---
+++ Source/ShootThemUp/Public/AI/EQS/STUEnemyEnvQueryContext.h
@@ -0,0 +1,23 @@
+// Shoot Them Up Game. All right reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryContext.h"
+#include "STUEnemyEnvQueryContext.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SHOOTTHEMUP_API USTUEnemyEnvQueryContext : public UEnvQueryContext
+{
+    GENERATED_BODY()
+
+public:
+    virtual void ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const;
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+    FName EnemyActorKeyName = "EnemyActor";
+};
