25d0e29b69ea4062b58696dc781e62c30fde1b0d
yadyginid <yadyginid@gmail.com>
2021-06-24 14:59:47 +0300

    Set spawn NPC for GameMode

--- Source/ShootThemUp/Public/STUGameModeBase.h
+++ Source/ShootThemUp/Public/STUGameModeBase.h
@@ -4,7 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "STUCoreTypes.h"
 #include "STUGameModeBase.generated.h"
+
+class AAIController;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUGameModeBase : public AGameModeBase
@@ -13,4 +16,20 @@ class SHOOTTHEMUP_API ASTUGameModeBase : public AGameModeBase
 
 public:
     ASTUGameModeBase();
+
+    virtual void StartPlay() override;
+    virtual UClass* GetDefaultPawnClassForController_Implementation(AController* InController) override;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<AAIController> AIControllerClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<APawn> AIPawnClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    FGameData GameData;
+
+private:
+    void SpawnBots();
 };
